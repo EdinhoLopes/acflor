2c5ae79a808ed0e0c46a09eaa94034ae6411fd1a
Edson Lopes <edsonlopesgomes7@gmail.com>
2024-07-25 16:32:40 -0300

    Melhoria no Layout do index e show de Student e Course

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,7 @@ class Student < ApplicationRecord
   has_many :student_histories, dependent: :destroy
   has_many :attendances, dependent: :destroy
 
-  validates :name, :date_of_birth, :registration_number, presence: true
-  validates :registration_number, uniqueness: true
+  validates :name, :date_of_birth, :address, :phone, :email, :registration_number, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, format: { with: /\A\+?[0-9]{10,15}\z/, message: "deve ser um número válido" }
 end
